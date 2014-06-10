9e90dc8f93f99429079ba085ba142cf5b4f4b89c
Matt Millar <matt@tellybug.com>
2014-06-10 12:06:23 +0100

    Trivial python package to download and install dynamoDB local

---
+++ README.txt
@@ -0,0 +1,10 @@
+Download and install DynamoDB local
+
+
+Create with
+
+python setup.py sdist
+
+install with
+
+pip install path/to/mypackage.tgz
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from setuptools.command.install import install
+import urllib
+import tarfile
+import os
+
+class CustomInstall(install):
+    def run(self):
+        install.run(self)
+        print "Installing latest dynamodblocal"
+        f, response = urllib.urlretrieve("http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest")
+        bundle = tarfile.open(f)
+        bundle.extractall(path=self.install_scripts)
+        os.remove(f)
+        print "extracted latest dynamodb local"
+
+setup(
+    name='dynamodblocal',
+    version='0.1dev',
+    packages=['dynamodblocal',],
+    license='The MIT License (MIT)',
+    long_description=open('README.txt').read(),
+    cmdclass={'install' : CustomInstall },
+    scripts=['scripts/dynamodblocal',]
+)
